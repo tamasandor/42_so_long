1a421b6d48614ab306a55b8bb9b77fc7a8b62bb6
atamas <atamas@student.42wolfsburg.de>
2024-04-24 14:07:11 +0200

    Norminette in progress

--- src/render.c
+++ src/render.c
@@ -6,51 +6,74 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 12:23:48 by atamas            #+#    #+#             */
-/*   Updated: 2024/04/24 12:28:31 by atamas           ###   ########.fr       */
+/*   Updated: 2024/04/24 14:06:09 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void render(t_vars *vars)
+void	assign_calculations(t_vars *vars, int *sx, int*sy, int *ex, int *ey)
 {
-    int x, y;
-    int viewport_size = 7;  // Number of tiles from the player to display in each direction
-    int tile_size = 64;     // Size of each tile in pixels
+	*sx = vars->mapchars->player_x - 7;
+	*sy = vars->mapchars->player_y - 14;
+	*ex = vars->mapchars->player_x + 29;
+	*ey = vars->mapchars->player_y + 14;
+	if (*sx < 0)
+		*sx = 0;
+	if (*sy < 0)
+		*sy = 0;
+	if (*ex >= vars->len_x)
+		*ex = vars->len_x - 1;
+	if (*ey >= vars->len_y)
+		*ey = vars->len_y - 1;
+	if ((vars->mapchars->player_x > (*ex - *sx) / 2) && *ex - 29 > -1)
+		*sx = *ex - 29;
+	if (vars->mapchars->player_y > 14 && *ey - 16 > -1)
+		*sy = *ey - 16;
+}
 
-    // Calculate viewport boundaries
-    int start_x = vars->mapchars->player_x - viewport_size;
-    int start_y = vars->mapchars->player_y - 14;
-    int end_x = vars->mapchars->player_x + 29;
-    int end_y = vars->mapchars->player_y + 14;
+void	render_x(t_vars *vars, int x, int y, void *img, int end_x, int start_x, int start_y)
+{
+	char	tile;
+	int		x_pos;
+	int		y_pos;
 
-    // Clamp the viewport to the map boundaries
-    start_x = (start_x < 0) ? 0 : start_x;
-    start_y = (start_y < 0) ? 0 : start_y;
-    end_x = (end_x >= vars->len_x) ? vars->len_x - 1 : end_x;
-    end_y = (end_y >= vars->len_y) ? vars->len_y - 1 : end_y;
-	start_x = ((vars->mapchars->player_x > (end_x - start_x) / 2) && end_x - 29 > -1) ? (end_x - 29) : start_x;
-	start_y = (vars->mapchars->player_y > 14 && end_y - 16 > -1) ? (end_y - 16) : start_y;
+	while (x <= end_x)
+	{
+		tile = vars->map[y][x];
+		img = NULL;
+		if (tile == '0')
+			img = vars->empty;
+		else if (tile == '1')
+			img = vars->wall;
+		else if (tile == 'C')
+			img = vars->money;
+		else if (tile == 'P')
+			img = vars->player;
+		x_pos = (x - start_x) * 64;
+		y_pos = (y - start_y) * 64;
+		if (img != NULL)
+			mlx_put_image_to_window(vars->mlx, vars->win, img, x_pos, y_pos);
+		x++;
+	}
+}
 
-    // Render only the tiles within the viewport
-    for (y = start_y; y <= end_y; y++)
-    {
-        for (x = start_x; x <= end_x; x++)
-        {
-            char tile = vars->map[y][x];
-            void *img = NULL;
-            if (tile == '0')
-                img = vars->empty;
-            else if (tile == '1')
-                img = vars->wall;
-            else if (tile == 'C')
-                img = vars->money;
-            else if (tile == 'P')
-                img = vars->player;
+void	render(t_vars *vars)
+{
+	int		x;
+	int		y;
+	int		start_x;
+	int		start_y;
+	int		end_x;
+	int		end_y;
+	char	tile;
 
-            if (img != NULL)
-                mlx_put_image_to_window(vars->mlx, vars->win, img, (x - start_x) * tile_size, (y - start_y) * tile_size);
-        }
-    }
-	// mlx_put_image_to_window(vars->mlx, vars->win, backg, 0, 0);
+	assign_calculations(vars, &start_x, &start_y, &end_x, &end_y);
+	y = start_y;
+	while (y <= end_y)
+	{
+		x = start_x;
+		render_x(vars, start_x, y, NULL, end_x, start_x, start_y);
+		y++;
+	}
 }
