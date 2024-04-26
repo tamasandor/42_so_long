6d23847e1b7f71c7e0a0e41baa440b7abff9accc
atamas <atamas@student.42wolfsburg.de>
2024-04-26 23:45:54 +0200

    Solve ft_printf issue

--- include/printf/ft_print.c
+++ include/printf/ft_print.c
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 19:32:10 by atamas            #+#    #+#             */
-/*   Updated: 2023/12/08 13:57:38 by atamas           ###   ########.fr       */
+/*   Updated: 2024/04/26 23:42:28 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	ft_buffer_zero(t_parser *parser)
 {
 	int	i;
 
-	i = 0;
-	while (parser->buffer[i] != '\0')
-		parser->buffer[i++] = '\0';
+	i = 32;
+	while (i > 0)
+		parser->buffer[i--] = '\0';
 }
 
 void	ft_printchr(t_parser *parser, char chr)
@@ -40,7 +40,6 @@ void	ft_printstr(t_parser *parser, char *str)
 			ft_printchr(parser, *str);
 			str++;
 		}
-	}
-	if (parser->buffer[0] != '\0')
 		ft_buffer_zero(parser);
+	}
 }
